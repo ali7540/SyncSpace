3cda8178f2ce30b11cf433cc5a52be530d461572
Asad Ali <asad24ali06@gmail.com>
2025-11-05 11:48:53 +0530

    changed prisma.js

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,3 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/client.ts";
+// import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaClient } from '@prisma/client';
 const prisma= new PrismaClient()
 export default prisma
